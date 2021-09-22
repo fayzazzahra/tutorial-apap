6b30eee5013cc48bed39a4e9f721dbfa51a2515f
Fayza Azzahra R <71934530+fayzazzahra@users.noreply.github.com>
2021-09-22 14:30:16 +0700

    feat/tutorial-2-21cineplux (#4)
    * feat/tutorial-2-21cineplux
    
    * feat/tutorial-2-21cineplux
    
    * feat/tutorial-2-21cineplux

---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/service/BioskopService.java
@@ -0,0 +1,19 @@
+package apap.tutorial.cineplux.service;
+
+import apap.tutorial.cineplux.model.BioskopModel;
+import java.util.List;
+
+public interface BioskopService {
+    //method untuk menambah bioskop
+    void addBioskop(BioskopModel bioskop);
+
+    //method untuk mendapatkan daftar bioskop yang telah tersimpan
+    List<BioskopModel> getBioskopList();
+
+    //method untuk mendapatkan data sebuah bioskop berdasarkan id bioskop
+    BioskopModel getBioskopByIdBioskop(String idBioskop);
+
+    //method untuk remove bioskop
+    void removeBioskop(BioskopModel bioskop);
+
+}
